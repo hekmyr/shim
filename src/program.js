ad78c0851d3a00744d7366e9752dccd42be7a2e6
hekmyr <163496286+hekmyr@users.noreply.github.com>
2025-06-19 23:44:03 +0200

    fix typo

--- src/program.js
+++ src/program.js
@@ -1,17 +1,17 @@
-const loadash = require('lodash');
+const lodash = require('lodash');
 
 const myString = "hello from target script!";
-const capitalizedString = loadash.capitalize(myString);
+const capitalizedString = lodash.capitalize(myString);
 
 console.log(`Target Script Says: Original: "${myString}"`);
 console.log(`Target Script Says: Capitalized by lodash: "${capitalizedString}"`);
 
 // Let's try a function that our fake lodash WON'T have
 try {
-  const aNumber = loadash.add(5, 3); // add is a real lodash function
-  console.log(`Target Script Says: loadash.add(5,3) result: ${aNumber}`);
+  const aNumber = lodash.add(5, 3); // add is a real lodash function
+  console.log(`Target Script Says: lodash.add(5,3) result: ${aNumber}`);
 } catch (e) {
-  console.error(`Target Script Says: Error calling loadash.add: ${e.message}`);
+  console.error(`Target Script Says: Error calling lodash.add: ${e.message}`);
 }
 
 module.exports = {
